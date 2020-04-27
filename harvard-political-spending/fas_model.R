74aa745b6de8bb7abcdea2f768ec35c2bfbce512
Andy Wang <azwang@college.harvard.edu>
2020-04-27 04:59:28 -0500

    finalized FAS model

---
+++ harvard-political-spending/fas_model.R
@@ -0,0 +1,38 @@
+# creating a dataframe for us to work with as we explore gender, race, school
+
+gender_race_school_data <- faculty_fec %>%
+  mutate(name = paste(last_name, ", ", first_name, sep = "")) %>%
+  group_by(name, title, school, gender, race) %>%
+  summarize(spending_sum = sum(contribution_receipt_amount)) %>%
+  ungroup() %>%
+  arrange(desc(spending_sum)) %>%
+  distinct(name, school, .keep_all = T)
+
+
+# creating breakdowns by each of the three variables we want to consider 
+
+uni_gender_breakdown <- gender_race_school_data %>%
+  group_by(gender) %>%
+  summarize(mean_spending = mean(spending_sum))
+
+uni_race_breakdown <- gender_race_school_data %>%
+  group_by(race) %>%
+  summarize(mean_spending = mean(spending_sum))
+
+uni_school_breakdown <- gender_race_school_data %>%
+  group_by(school) %>%
+  summarize(mean_spending = mean(spending_sum)) %>%
+  arrange(desc(mean_spending))
+
+# checking to see if race and gender interact
+
+uni_race_gender_breakdown <- gender_race_school_data %>%
+  group_by(race, gender) %>%
+  summarize(mean_spending = mean(spending_sum)) %>%
+  ungroup() %>%
+  arrange(desc(mean_spending))
+
+gender_race_school_model <- lm(
+  spending_sum ~ gender * race + school,
+  gender_race_school_data
+) 
