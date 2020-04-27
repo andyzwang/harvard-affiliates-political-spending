95500cf8257d793aab6d0d8d55b1b153e1d63203
Andy Wang <azwang@college.harvard.edu>
2020-04-26 22:55:12 -0500

    Imported code from original Gather.Rmd

---
+++ harvard-political-spending/data.R
@@ -0,0 +1,67 @@
+# importing the data from the faculty, as well as reading in the correct column
+# types.
+
+faculty <- read_csv("raw-data/faculty.csv", col_types = "ccffcc") %>%
+  clean_names()
+
+# reading in the FEC data (using a bulk search on the term "harvard", filtering
+# for years from 2017 - 2020, including the correct column types)
+
+fec <- read_csv("raw-data/fec1720.csv",
+  col_types = "ccicdccicffcccccccccccccciccccccccTddcccccccccccccccccccccccfcccccccccdcccccccc"
+)
+
+# cleaning up the faculty data: I first split the names into a full name, which
+# I then split into a last name and a first name. I also did a bunch of error
+# checking, since some people did in fact go by both names, or some people used
+# their middle names instead of their first names. I then selected out the
+# information that wasn't necessary. From there, I simplified to only use the
+# first first name given, since I can do that with the FEC data too to make sure
+# that no one is left out.
+
+faculty <- faculty %>%
+  mutate(
+    full_name = map(
+      name, ~ unlist(strsplit(., ","))
+    ),
+    last_name = as.character(map(
+      full_name, ~ .[1]
+    )),
+    full_first_name = map(
+      full_name, ~ .[2]
+    ),
+    first_split = map(
+      full_first_name, ~ unlist(
+        strsplit(., "\ ")
+      )
+    ),
+    first_name = as.character(
+      map(
+        first_split, ~ .[2]
+      )
+    )
+  ) %>%
+  select(last_name, first_name, title, gender, race, school, department)
+
+# used for error checking on last names to see if anyone has two last names
+
+# mutate(last_split = map(last_name, ~ unlist(strsplit(., "\ ")))) %>%
+# mutate(last2 = map(last_split, ~ .[2])) %>%
+# filter(!is.na(last2))
+
+# Now cleaning up the FEC data to put the information that we most need to the
+# front, as well as adding consistent column names so that merging is easier
+# later. Changed names' case from all-caps to title, so that it matches our
+# Harvard dataset.
+
+fec <- fec %>%
+  mutate(
+    first_split = map(
+      contributor_first_name, ~ unlist(strsplit(., "\ "))
+    ),
+    first_name = str_to_title(map(
+      first_split, ~ .[1]
+    )),
+    last_name = str_to_title(contributor_last_name)
+  ) %>%
+  select(last_name, first_name, everything())
