4f19e8d41aeacb930b96190cd6f4c9a0cc28a60c
Andy Wang <azwang@college.harvard.edu>
2020-04-27 02:51:41 -0500

    finished recipients tab

--- harvard-political-spending/app.R
+++ harvard-political-spending/app.R
@@ -38,14 +38,95 @@ ui <- navbarPage(
     titlePanel("Top Faculty Spenders"),
     p("Here's an aggregation of the sums of political spending by each individual faculty member from January 2017 until now."),
     p("The average spending across all faculty is", strong("$791.27"), "while the average spending for only those who spend is", strong("$2,896.57.")),
-    DTOutput("individualspending")
+    column(1),
+    column(
+      10,
+      DTOutput("individualspending")
+    ),
+    column(1)
   ),
   tabPanel(
     "Spending Recipients",
     tabsetPanel(
-      tabPanel("Recipients Overview"),
-      p("Here"),
-      DTOutput("recipient_overview")
+      tabPanel(
+        "Recipients Overview",
+        h2("Recipient Overview"),
+        p("Here's an overview of the committees to whom Harvard faculty donated between 2017-2020."),
+        column(1),
+        column(
+          10,
+          DTOutput("recipient_overview")
+        ),
+        column(1)
+      ),
+      tabPanel(
+        "Political Parties",
+        h2("Political Parties"),
+        p("Harvard faculty very strongly prefer spending money on Democratic candidates and organizations. Note: the Party count is not completely accurate, due to some partisan organizations with significant amounts of spending (i.e. ActBlue) not registering as partisan with the FEC."),
+        column(1),
+        column(
+          10,
+          plotOutput("party_spending")
+        ),
+        column(1)
+      ),
+      tabPanel(
+        "Party Spending Breakdown",
+        h2("Party Spending Breakdown"),
+        p("As evidenced by the individual party spending breakdown, Harvard faculty very much strongly prefer spending money on Democratic candidates and organizations. However, this table is very much incomplete: some partisan organizations do not register as such with the FEC, and as such, faculty who have spent on these organizations are not reflected in this chart."),
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              "party_spending_party",
+              "Select A Party",
+              c(
+                "Democratic" = "dem",
+                "Republican" = "gop",
+                "Democratic-Farmer-Labor" = "dfl",
+                "Independent" = "ind",
+                "Libertarian" = "lib"
+              )
+            )
+          ),
+          mainPanel(DTOutput("party_breakdown"))
+        )
+      ),
+      tabPanel(
+        "Geographic Spending",
+        h2("Geographic Spending"),
+        p("The political spending of Harvard faculty is dispersed throughout the entire continential U.S., but is seemed to be concentrated in a few key states: California, Illinois, Texas, and Massachusetts. Note that ActBlue has its headquarters in Mass, which significantly distorts the map. Select a region to get a better sense of spending within each region."),
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              "region_selection",
+              "Select A Region",
+              c(
+                "Entire U.S." = "us",
+                "New England" = "ne",
+                "Mid Atlantic" = "ma",
+                "East North Central" = "enc",
+                "South Atlantic" = "sa",
+                "East South Central" = "esc",
+                "West South Central" = "wsc",
+                "Mountain" = "m",
+                "Pacific" = "p"
+              )
+            )
+          ),
+          mainPanel(plotOutput("states"))
+        )
+      ),
+      tabPanel(
+        "Spending Over Time",
+        h2("Spending Over Time"),
+        p("Political spending by Harvard faculty seemed to pick up starting in January of 2017 (shortly before President Trump's inaguration) to a peak in November of 2018 (midterm elections). From there, it's been steadily rising again as we approach the 2020 elections."),
+        column(1),
+        column(
+          10,
+          plotOutput("spending_over_time")
+        ),
+        column(1)
+      )
     )
   ),
   tabPanel(
@@ -94,17 +175,10 @@ server <- function(input, output, session) {
   output$individualspending <- renderDataTable({
     datatable(spending_by_individual,
       options = list(
-        colnames = c(
-          "Name",
-          "Title",
-          "School",
-          "Department",
-          "Total Spending"
-        ),
         pageLength = 20
       )
     ) %>%
-      formatCurrency("spending_sum", "$")
+      formatCurrency("Sum of Spending", "$")
   })
 
   # including recipients data source
@@ -117,18 +191,158 @@ server <- function(input, output, session) {
   output$recipient_overview <- renderDataTable({
     datatable(spending_recipients_ui,
       options = list(
-        colnames = c(
-          "Committee Name",
-          "Spending Sum",
-          "Address",
-          "City",
-          "State",
-          "Party Affiliation"
-        ),
         pageLength = 20
       )
     ) %>%
-      formatCurrency("spending_sum", "$")
+      formatCurrency("Sum of Spending", "$")
+  })
+
+  # rendering a ggplot of party spending
+  output$party_spending <- renderPlot({
+    ggplot(parties, aes(x = party, y = total_spending)) +
+      geom_col(fill = "lightblue") +
+      theme_classic() +
+      scale_y_continuous(labels = scales::dollar) +
+      labs(
+        title = "Faculty Spendings to Each Party",
+        x = "Party",
+        y = "Sum of Spendings"
+      )
+  })
+
+  # render an adjustable table of party donations (responsive to input)
+  output$party_breakdown <- renderDataTable({
+
+    # setting the filter depending on what the input is
+
+    if (input$party_spending_party == "dem") {
+      donations_party_render <- individual_donations_party %>%
+        filter(Party == "DEM")
+    }
+    else if (input$party_spending_party == "gop") {
+      donations_party_render <- individual_donations_party %>%
+        filter(Party == "REP")
+    }
+    else if (input$party_spending_party == "dfl") {
+      donations_party_render <- individual_donations_party %>%
+        filter(Party == "DFL")
+    }
+    else if (input$party_spending_party == "ind") {
+      donations_party_render <- individual_donations_party %>%
+        filter(Party == "IND")
+    }
+    else if (input$party_spending_party == "lib") {
+      donations_party_render <- individual_donations_party %>%
+        filter(Party == "LIB")
+    }
+
+    datatable(donations_party_render,
+      options = list(
+        pageLength = 20
+      )
+    ) %>%
+      formatCurrency("Sum of Spending", "$")
+  })
+
+  # creating a map of spending data
+
+  output$states <- renderPlot({
+
+    # setting the region depending on what the input is
+
+    if (input$region_selection == "us") {
+      region_map <- plot_usmap(data = states, values = "total_spending")
+    }
+    else if (input$region_selection == "ne") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .new_england
+      )
+    }
+    else if (input$region_selection == "ma") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .mid_atlantic
+      )
+    }
+    else if (input$region_selection == "enc") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .east_north_central
+      )
+    }
+    else if (input$region_selection == "wnc") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .west_north_central
+      )
+    }
+    else if (input$region_selection == "sa") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .south_atlantic
+      )
+    }
+    else if (input$region_selection == "esc") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .east_south_central
+      )
+    }
+    else if (input$region_selection == "wsc") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .west_south_central
+      )
+    }
+    else if (input$region_selection == "m") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .mountain
+      )
+    }
+    else if (input$region_selection == "p") {
+      region_map <- plot_usmap(
+        data = states,
+        values = "total_spending",
+        include = .pacific
+      )
+    }
+
+    # plot final map
+
+    region_map +
+      scale_fill_continuous(
+        low = "white", high = "blue",
+        name = "Dollars Spent", label = scales::dollar
+      ) +
+      theme(legend.position = "right") +
+      labs(
+        title = "States Recieving Harvard Political Spending",
+        subtitle = "Using FEC Data from 2017-2020"
+      )
+  })
+  
+  # rendering a ggplot of spending over time
+  
+  output$spending_over_time <- renderPlot({
+    ggplot(spending_over_time, aes(x = month, y = donations)) +
+      geom_line() +
+      labs(
+        x = "Date",
+        y = "Total Spending",
+        title = "Total Harvard Political Spending Per Month"
+      ) +
+      theme_classic() +
+      scale_y_continuous(label = scales::dollar) 
   })
 }
 
